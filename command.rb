4cba3fb20b398d41b7884347683a52f77e2b3c4f
KhanradCoder <akhanrade@gmail.com>
2024-07-20 12:37:33 -0400

    format the table by removing parentheses from last column

--- command.rb
+++ command.rb
@@ -7,12 +7,33 @@ class Command
 
   AppProcess = Struct.new(:nothing, :jobid, :partition, :name, :user, :st, :time, :nodes, :nodelist)
 
+
+  # For the last column, nodelist
+  def remove_spaces_inside_parentheses(text)
+    text.gsub(/\((.*?)\)/) do |match|
+      inside_parentheses = match[1..-2].gsub(/\s+/, '')
+      "(#{inside_parentheses})"
+    end
+  end
+
+
   def parse(output)
     lines = output.strip.split("\n")
-    # Skip header lines
+    # Skip header line
     lines = lines.drop(1)
+    
     lines.map do |line|
-      fields = line.split(/\s{1,}/) # Split based on one or more spaces
+      #remove spaces inside of the parentheses, should there be any
+      line = remove_spaces_inside_parentheses(line)
+      # Split the line by whitespace
+      all_fields = line.split(/\s+/)
+      
+      first_three = all_fields[0, 3]
+      last_five = all_fields[-5, 5]
+      
+      name = all_fields[3..-6].join(' ')
+      fields = first_three + [name] + last_five
+      
       AppProcess.new(*fields)
     end
   end
